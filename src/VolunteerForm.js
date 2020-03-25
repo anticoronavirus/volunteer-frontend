9ff0af7ef2400cee54b405788570c6ac23eae654
jsus <esseswann@gmail.com>
2020-03-25 21:24:05 +0300

    Initial fields added

--- src/VolunteerForm.js
+++ src/VolunteerForm.js
@@ -1,7 +1,42 @@
 import { createElement as $ } from 'react'
+import Box from '@material-ui/core/Box'
 import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField'
 
-const VolunteerForm = () => 
-  $(Paper, null, 'VolunteerForm')
+const VolunteerForm = () =>
+  $(Box, { margin: 'auto', maxWidth: '60ex' }, 
+    $(Paper, null,
+      $(Box, { padding: 2 },
+        $(TextField, {
+          margin: 'normal',
+          label: 'Фамилия',
+          variant: 'outlined',
+          fullWidth: true
+        }),
+        $(TextField, {
+          margin: 'normal',
+          label: 'Имя',
+          variant: 'outlined',
+          fullWidth: true
+        }),
+        $(TextField, {
+          margin: 'normal',
+          label: 'Отчество',
+          variant: 'outlined',
+          fullWidth: true
+        }),
+        $(TextField, {
+          margin: 'normal',
+          label: 'Телефон',
+          variant: 'outlined',
+          fullWidth: true
+        }),
+        $(TextField, {
+          margin: 'normal',
+          label: 'Электропочта',
+          variant: 'outlined',
+          fullWidth: true
+        })
+        )))
 
 export default VolunteerForm
