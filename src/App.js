d53cafb4e2a854b8eaaaa3cb9a74535d8b4e8fbb
jsus <esseswann@gmail.com>
2020-03-25 21:05:41 +0300

    Initial volunteer form

--- src/App.js
+++ src/App.js
@@ -1,1 +1,9 @@
-export default () => 'App'
+import { createElement as $ } from 'react'
+import VolunteerForm from 'VolunteerForm'
+import { Switch, Route } from 'react-router-dom'
+
+const App = () =>
+  $(Switch, null,
+    $(Route, { path: '/volunteer', component: VolunteerForm }))
+
+export default App
