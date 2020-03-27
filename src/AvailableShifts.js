6a7069e12fbfeb4b8fa94f580aed4571e537dc4c
jsus <esseswann@gmail.com>
2020-03-27 17:14:24 +0300

    Separated Available shifts

---
+++ src/AvailableShifts.js
@@ -0,0 +1,107 @@
+import { createElement as $, memo } from 'react'
+import { useParams } from 'react-router-dom'
+import format from 'date-fns/format'
+import addDays from 'date-fns/addDays'
+import ru from 'date-fns/locale/ru'
+import map from 'lodash/fp/map'
+import entries from 'lodash/fp/entries'
+import reduce from 'lodash/fp/reduce'
+import find from 'lodash/fp/find'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Box from '@material-ui/core/Box'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Checkbox from '@material-ui/core/Checkbox'
+import { useSubscription, useMutation } from '@apollo/react-hooks'
+import {
+  shifts,
+  addVolunteerToShift,
+  removeVolunteerFromShift
+} from 'queries'
+
+const now = new Date()
+
+const AvailableShifts = memo(() => {
+
+  let { profession } = useParams()
+
+  const { data } = useSubscription(shifts, {
+    variables: {
+      profession,
+      from: now,
+      to: addDays(now, 14)
+    }
+  })
+
+  const generatedTable = data && reduce(generateTableReducer, {
+    columns: new Set(),
+    rows: {}
+  }, data.shifts)
+
+  return !data
+    ? $(Box, { padding: 2 }, $(CircularProgress))
+    : $(Table, null,
+        $(TableHead, null,
+          $(TableRow, null,
+            map(Header, Array.from(generatedTable.columns)))),
+        $(TableBody, null,
+          map(Row, entries(generatedTable.rows))))
+})
+
+const generateTableReducer = (result, shift) => {
+  result.columns.add(shift.date)
+  if (!result.rows[`${shift.start}-${shift.end}`])
+    result.rows[`${shift.start}-${shift.end}`] = []
+  result.rows[`${shift.start}-${shift.end}`].push(shift)
+  return result
+}
+
+const Header = date =>
+  $(TableCell, { key: date, style: { minWidth: '16ex' }}, format(new Date(date), 'd MMMM', { locale: ru }))
+
+const Row = ([key, cells]) =>
+  $(TableRow, { key }, map(CellFunction, cells))
+
+const CellFunction = cell => $(Cell, { key: cell.uid, ...cell })
+
+const Cell = ({
+  uid,
+  date,
+  start,
+  end,
+  volunteers,
+  professions
+}) => {
+  let { profession, volunteer_id } = useParams()
+
+  const required = find({ name: profession }, professions)
+  const me = find({ uid: volunteer_id }, volunteers)
+
+  const available = required ? required.number - volunteers.length : 0
+  const [mutate, { loading }] = useMutation(
+    me ? removeVolunteerFromShift : addVolunteerToShift,
+    { variables: { shift_id: uid, volunteer_id }})
+
+  return $(TableCell, { key: uid, style: { minWidth: '16ex' }},
+    $(Box, null, 
+      start.slice(0, 5), ' - ', end.slice(0, 5)),
+    $(Box, null, formatAvailable(available)),
+    loading
+      ? $(Box, { paddingTop: 1.1 }, $(CircularProgress, { size: 28 }))
+      : $(Box, { marginLeft: -1.5 },
+          $(Checkbox, { checked: !!me, disabled: !available, onClick: mutate })))
+}
+
+const formatAvailable = available =>
+  available === 0
+    ? 'нет мест'
+    : available === 1
+      ? '1 место'
+      : available > 4
+        ? `${available} мест`
+        : `${available} места`
+
+export default AvailableShifts
--- src/VolunteerForm.js
+++ src/VolunteerForm.js
@@ -1,35 +1,17 @@
-import { createElement as $, memo } from 'react'
+import { createElement as $ } from 'react'
 import Box from '@material-ui/core/Box'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-import map from 'lodash/fp/map'
-import entries from 'lodash/fp/entries'
-import reduce from 'lodash/fp/reduce'
-import find from 'lodash/fp/find'
-import { useSubscription, useMutation } from '@apollo/react-hooks'
 import Button from '@material-ui/core/Button'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Checkbox from '@material-ui/core/Checkbox'
 import { useTheme } from '@material-ui/core/styles'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
+import { useMutation } from '@apollo/react-hooks'
 import { useParams } from 'react-router-dom'
-import format from 'date-fns/format'
-import addDays from 'date-fns/addDays'
-import ru from 'date-fns/locale/ru'
 import { Formik, Form, Field } from 'formik'
 import MaskedInput from 'react-input-mask'
 import { TextField } from 'formik-material-ui'
-import {
-  addVolunteer,
-  shifts,
-  addVolunteerToShift,
-  removeVolunteerFromShift
-} from 'queries'
+import { addVolunteer } from 'queries'
+import AvailabeShifts from './AvailableShifts'
 
 const VolunteerForm = ({ history }) => {
 
@@ -115,7 +97,7 @@ const VolunteerForm = ({ history }) => {
     $(Box, { height: 16, minWidth: 16 }),
     data && data.insert_volunteer.returning[0].uid &&
       $(Paper, !matches && { style: { overflowX: 'scroll' }},
-        $(Shifts)))
+        $(AvailabeShifts)))
 }
 
 const PhoneField = ({
@@ -142,87 +124,5 @@ const initialValues = {
 }
 
 const required = value => !value && 'Обязательное поле'
-
-const now = new Date()
-
-const Shifts = memo(() => {
-
-  let { profession } = useParams()
-
-  const { data } = useSubscription(shifts, {
-    variables: {
-      profession,
-      from: now,
-      to: addDays(now, 14)
-    }
-  })
-
-  const generatedTable = data && reduce(generateTableReducer, {
-    columns: new Set(),
-    rows: {}
-  }, data.shifts)
-
-  return !data
-    ? $(Box, { padding: 2 }, $(CircularProgress))
-    : $(Table, null,
-        $(TableHead, null,
-          $(TableRow, null,
-            map(Header, Array.from(generatedTable.columns)))),
-        $(TableBody, null,
-          map(Row, entries(generatedTable.rows))))
-})
-
-const generateTableReducer = (result, shift) => {
-  result.columns.add(shift.date)
-  if (!result.rows[`${shift.start}-${shift.end}`])
-    result.rows[`${shift.start}-${shift.end}`] = []
-  result.rows[`${shift.start}-${shift.end}`].push(shift)
-  return result
-}
-
-const Header = date =>
-  $(TableCell, { key: date, style: { minWidth: '16ex' }}, format(new Date(date), 'd MMMM', { locale: ru }))
-
-const Row = ([key, cells]) =>
-  $(TableRow, { key }, map(CellFunction, cells))
-
-const CellFunction = cell => $(Cell, { key: cell.uid, ...cell })
-
-const Cell = ({
-  uid,
-  date,
-  start,
-  end,
-  volunteers,
-  professions
-}) => {
-  let { profession, volunteer_id } = useParams()
-
-  const required = find({ name: profession }, professions)
-  const me = find({ uid: volunteer_id }, volunteers)
-
-  const available = required ? required.number - volunteers.length : 0
-  const [mutate, { loading }] = useMutation(
-    me ? removeVolunteerFromShift : addVolunteerToShift,
-    { variables: { shift_id: uid, volunteer_id }})
-
-  return $(TableCell, { key: uid, style: { minWidth: '16ex' }},
-    $(Box, null, 
-      start.slice(0, 5), ' - ', end.slice(0, 5)),
-    $(Box, null, formatAvailable(available)),
-    loading
-      ? $(Box, { paddingTop: 1.1 }, $(CircularProgress, { size: 28 }))
-      : $(Box, { marginLeft: -1.5 },
-          $(Checkbox, { checked: !!me, disabled: !available, onClick: mutate })))
-}
-
-const formatAvailable = available =>
-  available === 0
-    ? 'нет мест'
-    : available === 1
-      ? '1 место'
-      : available > 4
-        ? `${available} мест`
-        : `${available} места`
 
 export default VolunteerForm
