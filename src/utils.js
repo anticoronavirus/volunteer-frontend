d97b51faaa84ff20e2eef736b90413c230e2733b
jsus <esseswann@gmail.com>
2020-04-02 18:37:56 +0300

    Universal formatLabel

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,32 @@ import format from 'date-fns/format'
 import ru from 'date-fns/locale/ru'
 
 export const formatDate = date => format(new Date(date), 'd MMMM', { locale: ru })
+
+export const formatLabel = (type, count) =>
+  count + ' ' +
+  labels[type].get(
+    count === 1
+      ? 1
+      : count < 20 && count > 5
+        ? 5
+        : count%10 < 5
+          ? 4
+          : 5)
+
+const labels = {
+  hospital: new Map([
+    [1, 'больница'],
+    [4, 'больницы'],
+    [5, 'больниц']
+  ]),
+  place: new Map([
+    [1, 'место'],
+    [4, 'места'],
+    [5, 'мест'],
+  ]),
+  volunteer: new Map([
+    [1, 'волонтёр'],
+    [4, 'волонтёра'],
+    [5, 'волонтёров'],
+  ]),
+} 
