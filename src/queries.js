41409890e5a07ac68a9751fef00dbd1c81091257
jsus <esseswann@gmail.com>
2020-04-04 01:06:13 +0300

    Comply to authenticated api

--- src/queries.js
+++ src/queries.js
@@ -182,8 +182,7 @@ export const submitPhone = gql`
 export const login = gql`
   mutation login($phone: String $password: String) {
     getToken(phone: $phone password: $password) {
-      accessToken
-      tokenType
+      authenticated
     }
   }
 `
