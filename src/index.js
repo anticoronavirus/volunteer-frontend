e94c013049cc341089e9e9ddcd3c9a8024d2451c
jsus <esseswann@gmail.com>
2020-04-04 19:19:25 +0300

    Yet another refresh token

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import { createElement as $, StrictMode } from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/react-components'
-import set from 'lodash/fp/set'
 import App from 'App.js'
-import { split, HttpLink, ApolloLink, ApolloClient, InMemoryCache } from '@apollo/client'
+import { setContext } from 'apollo-link-context'
+import { split, HttpLink, ApolloClient, InMemoryCache } from '@apollo/client'
 import { getMainDefinition } from '@apollo/client/utilities'
 import { WebSocketLink } from '@apollo/link-ws'
 import { refreshToken as query } from 'queries'
@@ -13,36 +13,46 @@ const httpLink = new HttpLink({
   uri: '/v1/graphql',
 })
 
-const authLink = new ApolloLink((operation, forward) => {
+const authLink = setContext((operation, { headers }) => {
+
   let authorization = localStorage.getItem('authorization')
   let expires = parseInt(localStorage.getItem('expires'))
 
-  if (expires && expires - new Date().valueOf() < 0)
-    fetch('/v1/graphql', {
+  if (!expires || !authorization)
+    return { headers }
+  else if (expires - new Date().valueOf() < 0)
+    return fetch('/v1/graphql', {
       method: 'POST',
-      body: JSON.stringify({ query })
-    }).then(response => response.json())
-      .then(({ data }) => {
-        authorization = localStorage.setItem('authorization', `Bearer ${data.refreshToken.accessToken}`)
-        expires = localStorage.setItem('expires', data.refreshToken.expires * 1000)
-        console.log(authorization, expires)
-        operation.setContext(({ headers }) =>
-          set(['headers', 'Authorization'],  authorization, headers))
-        forward(operation)
+      body: JSON.stringify({ query })})
+      .then(response => response.json())
+      .then(response => response.errors 
+        ? handleError(headers)
+        : response.data)
+      .then(data => {
+        localStorage.setItem('authorization', `Bearer ${data.refreshToken.accessToken}`)
+        localStorage.setItem('expires', data.refreshToken.expires * 1000)
+        return {
+          headers: {
+            ...headers,
+            Authorization: `Bearer ${data.refreshToken.accessToken}`,
+          }
+        }
       })
-      .catch(({ message }) => {
-        console.log(message)
-        localStorage.removeItem('authorization')
-        localStorage.removeItem('expires')
-      })
-  else {
-    if (authorization) {
-      operation.setContext(({ headers }) =>
-        set(['headers', 'Authorization'],  authorization, headers))
+      .catch(handleError(headers))
+  else
+    return {
+      headers: {
+        ...headers,
+        authorization
       }
-    return forward(operation)
-  }
+    }
 })
+
+const handleError = headers => {
+  localStorage.removeItem('authorization')
+  localStorage.removeItem('expires')
+  return headers
+}
 
 const wsLink = new WebSocketLink({
   uri: `wss://${process.env.NODE_ENV === 'development' ? 'dev.memedic.ru' : window.location.hostname}/v1/graphql`,
