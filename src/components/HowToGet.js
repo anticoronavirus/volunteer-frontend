e7976ea3baa80a03b6e4708bf8527df5d6ae961a
jsus <esseswann@gmail.com>
2020-04-18 14:53:08 +0300

    How to get checks

--- src/components/HowToGet.js
+++ src/components/HowToGet.js
@@ -3,25 +3,43 @@ import Markdown from 'components/Markdown'
 
 import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import TextField from '@material-ui/core/TextField'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 import { useMutation } from '@apollo/react-hooks'
 import { updateDirections } from 'queries'
 
 const HowToGet = ({ uid, directions }) => {
 
   const [source, setSource] = useState(directions)
+  const [tab, setTab] = useState('edit')
   const [mutate] = useMutation(updateDirections, { variables: { uid, directions: source }})
 
-  return $(Box, { padding: 2 },
-    $(TextField, {
-      label: 'Как добраться',
-      multiline: true,
-      fullWidth: true,
-      onChange: event => setSource(event.target.value),
-      value: source
-    }),
-    $(Button, { fullWidth: true, onClick: mutate }, 'Обновить'),
-    $(Markdown, { source }))
+  return $(ExpansionPanel, null,
+    $(ExpansionPanelSummary, null, 'Как добраться'),
+    $(ExpansionPanelDetails, null,
+      $(Box, null,
+        $(Tabs, { value: tab, onChange: (event, value) => setTab(value) },
+          $(Tab, { value: 'edit', label: 'Редактирование' }),
+          $(Tab, { value: 'preview', label: 'Просмотр' })),
+        tab === 'edit'
+          ? $(TextField, {
+              multiline: true,
+              fullWidth: true,
+              onChange: event => setSource(event.target.value),
+              value: source
+            })
+          : $(Markdown, { source }),
+        $(Box, { height: 24 }),
+        $(Button, {
+          variant: 'outlined',
+          fullWidth: true,
+          disabled: source === directions,
+          onClick: mutate
+        }, 'Обновить'))))
 }
 
 export default HowToGet
