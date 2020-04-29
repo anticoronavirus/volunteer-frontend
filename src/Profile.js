ab06ac959324cc291343968f3da49fc80c424c05
jsus <esseswann@gmail.com>
2020-04-29 14:56:47 +0300

    ProfileForm mutation

--- src/Profile.js
+++ src/Profile.js
@@ -17,6 +17,7 @@ import map from 'lodash/fp/map'
 import entries from 'lodash/fp/entries'
 
 import CircularProgress from '@material-ui/core/CircularProgress'
+import Divider from '@material-ui/core/Divider'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import Box from '@material-ui/core/Box'
@@ -55,7 +56,6 @@ const ProfilePure = data =>  {
   const { page = '' } = useParams()
   const { push } = useHistory()
   const notMobile = useMediaQuery(theme.breakpoints.up('sm'))
-  const [mutate] = useMutation(updateVolunteer, { variables: { uid: data.uid }})
   const history = useHistory()
 
   // FIXME mui-org/material-ui#9337
@@ -71,14 +71,16 @@ const ProfilePure = data =>  {
         $(Box, {
           display: 'flex',
           alignItems: 'center',
-          justifyContent: 'center',
-          height: 140 }, 
+          padding: 3,
+          justifyContent: 'center'}, 
           $(Avatar, { style: { width: 84, height: 84 } })),
           $(Box, { display: 'flex', justifyContent: 'center', alignItems: 'center' },
             $(Typography, { variant: 'subtitle1', align: 'center' }, data.phone),
             $(Tooltip, { title: 'Выход' },
               $(Button, { onClick: () => logoff() && history.push('/')},
               $(ExitToApp, { fontSize: 'small' })))),
+      $(Box, { height: 16 }),
+      $(Divider),
       $(Tabs, {
         variant: 'fullWidth',
         value: page,
@@ -87,6 +89,7 @@ const ProfilePure = data =>  {
         map(([value, { label }]) =>
           $(Tab, { id: value, key: value, value, label }),
           entries(tabs)))),
+        $(Box, { height: 16 }),
         $(Paper, null,
           $(Switch, null,
             map(([value, { component }]) =>
@@ -106,103 +109,109 @@ const ProfilePure = data =>  {
                 // ))
 }
 
+const ProfileForm = data => {
+  const [mutate] = useMutation(updateVolunteer, { variables: { uid: data.uid }})
+  const history = useHistory()
+
+  return $(Box, { padding: 2 },
+    $(Formik, {
+      initialValues: {
+        ...data,
+        car: '',
+        licenceplate: ''
+      },
+      // validateOnBlur: false,
+      validateOnMount: true,
+      onSubmit: variables =>
+        mutate({ variables: requiredProfileFields(variables) })
+          .then(() => history.push('/'))
+        },
+      ({ submitForm, isValid, dirty }) =>
+        $(Form, null, 
+          $(Field, {
+            component: TextField,
+            name: 'lname',
+            validate: required,
+            label: 'Фамилия',
+            margin: 'normal',
+            fullWidth: true,
+            helperText: ' ',
+            variant: 'outlined' }),
+          $(Field, {
+            component: TextField,
+            name: 'fname',
+            validate: required,
+            label: 'Имя',
+            margin: 'normal',
+            fullWidth: true,
+            helperText: ' ',
+            variant: 'outlined' }),
+          $(Field, {
+            component: TextField,
+            name: 'mname',
+            validate: required,
+            label: 'Отчество',
+            margin: 'normal',
+            fullWidth: true,
+            helperText: ' ',
+            variant: 'outlined' }),
+          // $(Field, {
+          //   component: FormikButtonGroup,
+          //   name: 'profession' }),
+          $(Field, {
+            component: TextField,
+            name: 'comment',
+            validate: required,
+            label: 'Специализация',
+            margin: 'normal',
+            fullWidth: true,
+            helperText: ' ',
+            placeholder: 'Например студент-онколог',
+            variant: 'outlined' }),
+          $(Field, {
+            component: TextField,
+            name: 'email',
+            validate: value => value.match(/^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/) ? null : 'Введите корректную почту',
+            label: 'Электронная почта',
+            margin: 'normal',
+            fullWidth: true,
+            helperText: ' ',
+            variant: 'outlined' }),
+          $(Field, {
+            component: TextField,
+            name: 'car',
+            label: 'Марка машины',
+            validate: value => value && value.length > 16 && 'Сократите описание',
+            margin: 'normal',
+            fullWidth: true,
+            helperText: ' ',
+            variant: 'outlined' }),
+          $(Field, {
+            component: TextField,
+            name: 'licenceplate',
+            label: 'Номер машины',
+            validate: value => value !== '_ ___ __ __' && value.match('_') && 'Заполните номер целиком, ключая регион',
+            margin: 'normal',
+            fullWidth: true,
+            helperText: ' ',
+            variant: 'outlined',
+            InputProps: {
+              inputComponent: LicensePlate
+            } }),
+          $(Box),
+          isValid
+            ? $(Button, {
+                onClick: submitForm,
+                variant: 'outlined',
+                fullWidth: true,
+              }, 'Сохранить')
+            : $(Box, { padding: '8px 0' }, $(Typography, { variant: 'caption' }, 'Пожалуйста, заполните все поля')))))
+}
+
 const tabs = {
   '': {
     label: 'Профиль',
-    component: data =>
-    $(Box, { padding: 2 },
-      $(Formik, {
-        initialValues: {
-          ...data,
-          car: '',
-          licenceplate: ''
-        },
-        // validateOnBlur: false,
-        validateOnMount: true,
-        // onSubmit: variables =>
-        //   mutate({ variables: requiredProfileFields(variables) })
-        //     .then(() => history.push('/'))
-          },
-        ({ submitForm, isValid, dirty }) =>
-          $(Form, null, 
-            $(Field, {
-              component: TextField,
-              name: 'lname',
-              validate: required,
-              label: 'Фамилия',
-              margin: 'normal',
-              fullWidth: true,
-              helperText: ' ',
-              variant: 'outlined' }),
-            $(Field, {
-              component: TextField,
-              name: 'fname',
-              validate: required,
-              label: 'Имя',
-              margin: 'normal',
-              fullWidth: true,
-              helperText: ' ',
-              variant: 'outlined' }),
-            $(Field, {
-              component: TextField,
-              name: 'mname',
-              validate: required,
-              label: 'Отчество',
-              margin: 'normal',
-              fullWidth: true,
-              helperText: ' ',
-              variant: 'outlined' }),
-            // $(Field, {
-            //   component: FormikButtonGroup,
-            //   name: 'profession' }),
-            $(Field, {
-              component: TextField,
-              name: 'comment',
-              validate: required,
-              label: 'Специализация',
-              margin: 'normal',
-              fullWidth: true,
-              helperText: ' ',
-              placeholder: 'Например студент-онколог',
-              variant: 'outlined' }),
-            $(Field, {
-              component: TextField,
-              name: 'email',
-              validate: value => value.match(/^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/) ? null : 'Введите корректную почту',
-              label: 'Электронная почта',
-              margin: 'normal',
-              fullWidth: true,
-              helperText: ' ',
-              variant: 'outlined' }),
-            $(Field, {
-              component: TextField,
-              name: 'car',
-              label: 'Марка машины',
-              validate: value => value && value.length > 16 && 'Сократите описание',
-              margin: 'normal',
-              fullWidth: true,
-              helperText: ' ',
-              variant: 'outlined' }),
-            $(Field, {
-              component: TextField,
-              name: 'licenceplate',
-              label: 'Номер машины',
-              validate: value => value !== '_ ___ __ __' && value.match('_') && 'Заполните номер целиком, ключая регион',
-              margin: 'normal',
-              fullWidth: true,
-              helperText: ' ',
-              variant: 'outlined',
-              InputProps: {
-                inputComponent: LicensePlate
-              } }),
-            $(Box, { height: 16 }),
-            isValid
-              ? $(Button, {
-                  onClick: submitForm,
-                  variant: 'outlined',
-                  fullWidth: true,
-                }, 'Сохранить')
-              : $(Box, { padding: '8px 0' }, $(Typography, { variant: 'caption' }, 'Пожалуйста, заполните все поля')))))
+    component: ProfileForm
   },
   shifts: {
     label: 'Смены',
