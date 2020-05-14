8829d9a97d18f9727b71385d798fbf4d7caaa3d0
jsus <esseswann@gmail.com>
2020-05-14 10:58:21 +0300

    Initial requirements refactoring

--- src/Hospital/ShiftDialog/Requirements.js
+++ src/Hospital/ShiftDialog/Requirements.js
@@ -1,4 +1,4 @@
-import { createElement as $, useState, memo } from 'react'
+import { createElement as $, memo } from 'react'
 import MenuItem from '@material-ui/core/MenuItem'
 import Chip from '@material-ui/core/Chip'
 import { useQuery } from '@apollo/react-hooks'
@@ -6,66 +6,49 @@ import {
   requirements as requirementsQuery
 } from 'queries'
 import map from 'lodash/fp/map'
-import filter from 'lodash/fp/find'
 import Box from '@material-ui/core/Box'
 import { styled } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 
 const Requirements = ({
-  selected,
   hospitalId,
   professionId,
-  onChange
+  value,
+  onChange = console.log
 }) => {
-  const requirementsResult = useQuery(requirementsQuery, { skip: !professionId, variables: {
-    where: {
-      hospital_id: { _eq: hospitalId  },
-      profession_id: { _eq: professionId }
-    }
-  }})
 
-  const [requirements, setRequirements] = useState(map('uid', selected) || [])
+  const requirementsResult = useQuery(requirementsQuery, {
+    variables: {
+      where: {
+        hospital_id: { _eq: hospitalId  },
+        profession_id: { _eq: professionId }
+      }
+    }})
 
-  const handleChange = (event) => {
-    console.log(event.target.value)
-    setRequirements(event.target.value)
-    onChange(requirementsResult.data.requirements.filter(i => event.target.value.includes(i.uid)))
-  }
-
-  return !requirementsResult.loading && requirementsResult.data &&
-    $(Box, { marginTop: 3 },
-      $(TextField, {
-        value: requirements,
-        onChange: handleChange,
-        select: true,
-        label: 'Обязательные условия',
-        variant: 'outlined',
-        fullWidth: true,
-        SelectProps: {
-          multiple: true,
-          renderValue: () =>
-            $(Chips, null,
-              map(requirement =>
-                $(StyledChip, { key: requirement.uid, label: requirement.name }),
-                selected
-            ))
-        },
+  return $(Box, { marginTop: 3 },
+    $(TextField, {
+      onChange,
+      size: value && value.length && 'small',
+      value: map('requirement.uid', value),
+      select: true,
+      label: 'Обязательные условия',
+      variant: 'outlined',
+      fullWidth: true,
+      SelectProps: {
+        multiple: true,
+        renderValue: () => map(SelectedRequirement, value)
       },
-        map(requirement =>
-          $(MenuItem, { key: requirement.uid, value: requirement.uid }, requirement.name),
-          requirementsResult.data.requirements
-        )))
+    },
+      requirementsResult.data &&
+        map(Requirement, requirementsResult.data.requirements)))
 }
 
-Requirements.defaultProps = {
-  onChange: () => {}
-}
+const SelectedRequirement = ({ uid, requirement }) =>
+  $(StyledChip, { key: uid, label: requirement.name })
 
-const Chips = styled('div')({
-  display: 'flex',
-  flexWrap: 'wrap'
-})
+const Requirement = ({ name, uid }) =>
+  $(MenuItem, { key: uid, value: uid }, name)
 
-const StyledChip = styled(Chip)({ margin: 3 })
+const StyledChip = styled(Chip)({ margin: 1 })
 
 export default memo(Requirements)
