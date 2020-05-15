ac5ee60c080a1e7607bee3bc0fba0d681c2829e7
Jenya Trofimov <deadrime@yandex.ru>
2020-05-15 17:17:45 +0300

    Add CircularProgress icon when loading professionsQuery

--- src/Hospital/ShiftDialog/Professions.js
+++ src/Hospital/ShiftDialog/Professions.js
@@ -7,34 +7,39 @@ import map from 'lodash/fp/map'
 import find from 'lodash/fp/find'
 import TaskOption from 'components/TaskOption'
 import TextField from '@material-ui/core/TextField'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Box from '@material-ui/core/Box'
 
 const Professions = ({
   selected,
   onChange
 }) => {
-  const professionsResult = useQuery(professionsQuery)
+  const { data, loading } = useQuery(professionsQuery)
 
-  return !professionsResult.loading && professionsResult.data &&
-    $(TextField, {
+  return $(TextField, {
       value: selected,
       onChange: event => {
-        onChange(find({ uid: event.target.value }, professionsResult.data.professions))
+        onChange(find({ uid: event.target.value }, data.professions))
       },
       select: true,
       label: 'Профессия',
       variant: 'outlined',
       fullWidth: true,
       SelectProps: {
+        ...!data && loading && { IconComponent },
         renderValue: () => $(Selected, selected)
       }
     },
-      map(profession =>
-        TaskOption({
-          onClick: () => {},
-          ...profession
-        }),
-        professionsResult.data.professions))
+      data &&
+        map(profession =>
+          TaskOption({
+            onClick: () => {},
+            ...profession
+          }),
+          data.professions))
 }
+
+const IconComponent = () => $(Box, { paddingRight: 1.5, paddingTop: 1, }, $(CircularProgress, { size: 16 }))
 
 const Selected = ({ name }) => {
   return $('div', null, name)
