e7c7aa43647ea8a732e0e4ebf86bf7fc651ba1e5
jsus <esseswann@gmail.com>
2020-09-16 23:41:11 +0300

    Initial shift data

--- src/Hospital/Shifts/VolunteerView.js
+++ src/Hospital/Shifts/VolunteerView.js
@@ -1,16 +1,19 @@
 import { useQuery } from '@apollo/react-hooks'
 import DateFnsUtils from '@date-io/date-fns'
 import { Paper } from '@material-ui/core'
-import { Box, Button, ListSubheader, List, ListItem, ListItemText, TextField, Typography } from '@material-ui/core'
+import { Box, Button, List, ListItem, ListItemText, ListSubheader, MenuItem, TextField, Typography } from '@material-ui/core'
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab'
 import { DatePicker, MuiPickersUtilsProvider, TimePicker } from '@material-ui/pickers'
+import addHours from 'date-fns/fp/addHours'
+import differenceInHours from 'date-fns/fp/differenceInHours'
 import ruLocale from 'date-fns/locale/ru'
 import map from 'lodash/fp/map'
-import some from 'lodash/fp/some'
 import range from 'lodash/fp/range'
+import some from 'lodash/fp/some'
 import { createElement as $, Fragment, useState } from 'react'
 
-import { hospitalRequirements } from 'queries'
+import TaskOption from 'components/TaskOption'
+import { hospitalRequirements, professions } from 'queries'
 
 import Onboarding from './Onboarding'
 
@@ -30,7 +33,7 @@ const VolunteerView = ({
   
   return requirementsSatisfied
     ? $(Fragment, null,
-        $(RequestShift),
+        $(RequestShift, { hospitalId }),
         $(Box, { padding: 1 }),
         $(Paper, null,
           $(List, null,
@@ -48,8 +51,14 @@ const VolunteerView = ({
     : $(Onboarding, data)
 }
 
-const RequestShift = () =>
-  $(Paper, null,
+const RequestShift = ({
+  hospitalId
+}) => {
+
+  const [data, setData] = useState({})
+  const professionQuery = useQuery(professions, { hospitalId })
+
+  return $(Paper, null,
     $(Box, { padding: 2 },
       $(MuiPickersUtilsProvider, { utils: DateFnsUtils, locale: ruLocale },
         $(Typography, { variant: 'h5', paragraph: true },
@@ -61,10 +70,10 @@ const RequestShift = () =>
             disablePast: true,
             fullWidth: true,
             format: 'dd MMMM',
-            value: null,
+            value: data.date || null,
             size: 'small',
             inputVariant: 'outlined',
-            onChange: console.log,
+            onChange: (date) => setData({ ...data, date }),
             disableToolbar: true }),
           $(Box, { padding: 1 }),
           $(TimePicker, {
@@ -72,15 +81,16 @@ const RequestShift = () =>
             fullWidth: true,
             disablePast: true,
             size: 'small',
-            value: null,
-            onChange: console.log,
+            value: data.start || null,
+            onChange: (start) => setData({ ...data, start }),
             ampm: false,
             inputVariant: 'outlined',
           })),
           $(Box, { marginTop: 2 },
             $(ToggleButtonGroup, {
-              value: 4,
+              value: data.duration,
               size: 'small',
+              onChange: (event, duration) => setData({ ...data, duration }),
               exclusive: true },
               toggleButtons)),
           $(TextField, {
@@ -89,15 +99,20 @@ const RequestShift = () =>
             fullWidth: true,
             select: true,
             size: 'small',
-            variant: 'outlined'
-          }),
+            variant: 'outlined',
+            value: data.profession_id || '',
+            onChange: event => setData({ ...data, profession_id: event.target.value })
+          }, map(ProfesionItem, professionQuery?.data?.professions)),
           $(Box, { marginTop: 1 },
             $(Button, {
-              disabled: true,
+              disabled: !data.profession_id || !data.date || !data.start || !data.duration,
               color: 'primary',
               onChange: console.log,
               variant: 'contained' },
             'Отправить заявку')))))
+}
+
+const ProfesionItem = ({ uid, name }) => $(MenuItem, { key: uid, value: uid }, name)
 
 const renderToggleButton = (key) =>
   $(ToggleButton, { key, value: (key * 2) + 4 }, (key * 2) + 4, ' часов')
