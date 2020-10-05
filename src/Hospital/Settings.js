979d9c56514fbcee469880270e839255ce2fe366
jsus <esseswann@gmail.com>
2020-10-05 14:47:54 +0300

    Cleanup

--- src/Hospital/Settings.js
+++ src/Hospital/Settings.js
@@ -1,15 +1,15 @@
 import { useQuery } from '@apollo/client'
 import { List, ListItem, ListItemText, Paper } from '@material-ui/core'
 import map from 'lodash/fp/map'
-import { createElement as $, useContext } from 'react'
+import { createElement as $, /* useContext */ } from 'react'
 
 import { professions } from 'queries'
 
-import HospitalContext from './HospitalContext'
+// import HospitalContext from './HospitalContext'
 import SubheaderWithBackground from 'components/SubheaderWithBackground'
 
 const Settings = () => {
-  const { data, error } = useQuery(professions)
+  const { data } = useQuery(professions)
   return $(Paper, null,
     $(List, null,
       $(SubheaderWithBackground, null, 'Доступные профессии'),
