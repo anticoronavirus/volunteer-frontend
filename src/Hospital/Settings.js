ca6ecbe839867880dae798cbbe25fd7ad0dee137
jsus <esseswann@gmail.com>
2020-10-07 15:09:02 +0300

    Initial professions list

--- src/Hospital/Settings.js
+++ src/Hospital/Settings.js
@@ -1,15 +1,24 @@
 import { useQuery } from '@apollo/client'
 import { List, ListItem, ListItemText, Paper } from '@material-ui/core'
 import map from 'lodash/fp/map'
-import { createElement as $, /* useContext */ } from 'react'
+import { createElement as $, useContext } from 'react'
 
+import SubheaderWithBackground from 'components/SubheaderWithBackground'
 import { professions } from 'queries'
 
-// import HospitalContext from './HospitalContext'
-import SubheaderWithBackground from 'components/SubheaderWithBackground'
+import HospitalContext from './HospitalContext'
 
 const Settings = () => {
-  const { data } = useQuery(professions)
+  const { hospitalId } = useContext(HospitalContext)
+  const { data } = useQuery(professions, {
+    variables: {
+      where: {
+        hospital_profession: {
+          hospital_id: { _eq: hospitalId }
+        }
+      }
+    }
+  })
   return $(Paper, null,
     $(List, null,
       $(SubheaderWithBackground, null, 'Доступные профессии'),
